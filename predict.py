f97d4f58e5e9c1117d461828e8b828bd9d171855
binhng2803 <binhng2803@gmail.com>
2023-10-20 03:03:58 +0700

    add predict.py

---
+++ predict.py
@@ -0,0 +1,22 @@
+from vgg19_model import VGG19_net
+import cv2
+from transforms import *
+import torch
+
+
+model = VGG19_net()
+model.load_state_dict(torch.load('model/best.pt'))
+
+def predict(img):
+    img = test_transform(img)
+    img = img.unsqueeze(0)
+    with torch.no_grad():
+        outputs = model(img)
+        predictions = torch.argmax(outputs, dim=1)
+        return predictions.item()
+    
+img = cv2.imread('data/test/cat/cat_0011.jpg')
+print(predict(img))
+
+
+
